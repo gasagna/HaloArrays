a64759ba63446ee51c8e2cbfbede06fcb099e4a5
Davide Lasagna <davide.lasagna@soton.ac.uk>
2018-03-19 19:39:37 +0000

    added some 2D tests

--- tests/parallel/src/test_constructor.cpp
+++ tests/parallel/src/test_constructor.cpp
@@ -5,23 +5,47 @@
 
 TEST_CASE("2D tests", "[2D tests]") {
 
-    std::array<int, 2> proc_grid_size = {2, 2};
-    std::array<int, 2> array_size = {20, 20};
+    std::array<int, 2> proc_grid_size = {3, 4}; // to be run on 12 processors
+    std::array<int, 2> array_size = {30, 40};
     std::array<int, 2> isperiodic = {false, false};
     std::array<int, 2> nhalo_out = {0, 0};
     int nhalo_in = 2;
 
-    DArrays::DArraySpec<2> spec(MPI_COMM_WORLD,
-                                proc_grid_size,
-                                array_size,
-                                isperiodic, nhalo_out, nhalo_in);
+    DArrays::DArray<double, 2> u(MPI_COMM_WORLD,
+                                 proc_grid_size,
+                                 array_size,
+                                 isperiodic, nhalo_out, nhalo_in);
 
-    DArrays::DArray<double, 2> u(spec);
+    SECTION("inbound indexing") {
+        for (auto [i, j] : u.indices()) {
+            u(i, j) = i*j;
+            REQUIRE( u(i, j) == i*j );
+        }
+    }
 
-    for (const  auto& [i, j] : u.indices()) {
-        u(i, j) = i*j;
-        REQUIRE( u(i*j) == i*j );
+    SECTION("out of bounds indexing") {
+        if (u.is_on_left_boundary(0) == true )
+            REQUIRE_THROWS( u(-1, 0) = 1 );
 
+        if (u.is_on_right_boundary(0) == true )
+            REQUIRE_THROWS( u(10, 0) = 1 );
+
+        if (u.is_on_left_boundary(1) == true)
+            REQUIRE_THROWS( u(0, -1) = 1 );
+
+        if (u.is_on_right_boundary(1) == true)
+            REQUIRE_THROWS( u(0, 10) = 1 );
+
+        if (!u.is_on_boundary() == true) {
+            REQUIRE_NOTHROW( u(-1,  0) = 1 );
+            REQUIRE_NOTHROW( u(10,  0) = 1 );
+            REQUIRE_NOTHROW( u( 0, -1) = 1 );
+            REQUIRE_NOTHROW( u( 0, 10) = 1 );
+            REQUIRE_NOTHROW( u(-1, -1) = 1 );
+            REQUIRE_NOTHROW( u(10, 10) = 1 );
+            REQUIRE_NOTHROW( u(10, -1) = 1 );
+            REQUIRE_NOTHROW( u(-1, 10) = 1 );
+        }
     }
 
 }
