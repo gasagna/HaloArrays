7f23c17a8be6032ef015c608e5c3a9f5f0fb5d68
Davide Lasagna <davide.lasagna@soton.ac.uk>
2018-03-19 17:47:18 +0000

    added test infrastructure for parallel computations

--- tests/parallel/src/runtests.cpp
+++ tests/parallel/src/runtests.cpp
@@ -1,23 +1,11 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
 #include "DArrays.hpp"
-#include <array>
 
-int main (int argc, char* argv[]) {
 
+int main(int argc, char* argv[]) {
     DArrays::MPISession::Initialize();
-
-    std::array<int, 2> proc_grid_size = {    2,     2};
-    std::array<int, 2>     array_size = {   20,    20};
-    std::array<int, 2>     isperiodic = {false, false};
-    std::array<int, 2>      nhalo_out = {    0,     0};
-    int                      nhalo_in =             2;
-
-    DArrays::DArraySpec<2> spec(MPI_COMM_WORLD, 
-                                proc_grid_size, 
-                                array_size, 
-                                isperiodic, nhalo_out, nhalo_in);
-
-    DArrays::DArray<double, 2> u(spec);
-
+    const int result = Catch::Session().run(argc, argv);
     DArrays::MPISession::Finalize();
-
+    return result;
 }
