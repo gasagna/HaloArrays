c0b299d80d0cee0a0fc702cba41056d16ffc96b9
Davide Lasagna <davide.lasagna@soton.ac.uk>
2018-03-27 22:37:45 +0100

    send subarray using MPI datatype

--- include/mpiwrapper.hpp
+++ include/mpiwrapper.hpp
@@ -28,3 +28,26 @@ int message_tag(HaloRegion<NDIMS> region) {
         tag += std::pow(10, dim+1)*region[dim];
     return tag;
 }
+
+////////////////////////////////////////////////////////
+//                 SEND/RECV SUBARRAYS                //
+////////////////////////////////////////////////////////
+template <typename T, size_t NDIMS>
+void send(subarray<T, NDIMS> sub, int dest, MESSAGETAG tag) {
+    MPI_Datatype type_send;
+    MPI_Type_create_subarray(PARENTNDIMS,                // parent number of dimensions
+                             sub.parent().size().data(), // parent size
+                             sub.size().data(),          // subarray size
+                             sub.raw_origin().data(),    // coordinate of start
+                             MPI_ORDER_FORTRAN,          // we have column major data
+                             T, &type_send);
+    MPI_Type_commit(&type_send);
+    MPI_Send(sub.parent().data(),
+             1,
+             type_send,
+             dest,
+             tag,
+             sub.parent().communicator());
+    MPI_Type_free(&type_send);
+}
+}
