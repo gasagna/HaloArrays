666c931fef72cd444e2b4f2224524c65d99aada9
Davide Lasagna <davide.lasagna@soton.ac.uk>
2018-03-27 22:37:17 +0100

    create unique message tag for each halo region

--- include/mpiwrapper.hpp
+++ include/mpiwrapper.hpp
@@ -13,3 +13,18 @@ inline void Initialize() {
 inline void Finalize() {
     MPI_Finalize();
 }
+
+
+////////////////////////////////////////////////////////
+//                   MESSAGE TAGS                     //
+////////////////////////////////////////////////////////
+
+// I hate seeing 0s and 1s around. This function constructs
+// a unique message tag for each halo region.
+template<size_t NDIMS>
+int message_tag(HaloRegion<NDIMS> region) {
+    int tag = 0;
+    for ( auto dim : LinRange(NDIMS) )
+        tag += std::pow(10, dim+1)*region[dim];
+    return tag;
+}
