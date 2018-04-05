4a8eee2c6fc484584800561457a42af3d54bb268
Davide Lasagna <davide.lasagna@soton.ac.uk>
2018-04-05 19:55:07 +0100

    modified variable name and member function name

--- include/dlayout.hpp
+++ include/dlayout.hpp
@@ -18,11 +18,11 @@ class DArrayLayout {
 // VARIABLES
 private:
     std::map<HaloRegion<NDIMS>, int> _rank_of_neighbour_at_map; // ranks of neighbouring processors
-    std::array<int, NDIMS>                   _proc_grid_coords; // coordinates of current processor in the grid
-    std::array<int, NDIMS>                     _proc_grid_size; // the size of the processor grid over which data is distributed
     std::array<int, NDIMS>                        _is_periodic; // whether the processor grid should wrap around
     int                                             _comm_size; // the number of processors in the communicator
     int                                             _comm_rank; // rank of current processor within communicator
+    std::array<int, NDIMS>                             _coords; // coordinates of current processor in the grid
+    std::array<int, NDIMS>                               _size; // the size of the processor grid over which data is distributed
     MPI_Comm                                             _comm; // communicator connecting all processor over which the array data is distributed
 
 // FUNCTIONS
@@ -57,9 +57,9 @@ private:
         if (_is_periodic[dim]) return true;
         switch(tag) {
             case HaloRegionTag::LEFT:
-                return _proc_grid_coords[dim] != 0;
+                return _coords[dim] != 0;
             case HaloRegionTag::RIGHT:
-                return _proc_grid_coords[dim] != _proc_grid_size[dim] - 1;
+                return _coords[dim] != _size[dim] - 1;
             case HaloRegionTag::CENTER:
                 return true;
         }
@@ -76,27 +76,26 @@ public:
     // ===================================================================== //
     // CONSTRUCTOR
     DArrayLayout(MPI_Comm comm, 
-                   std::array<int, NDIMS> proc_grid_size,
-                   std::array<int, NDIMS> is_periodic) 
-        : _proc_grid_size   (proc_grid_size  )
-        , _is_periodic      (is_periodic) {
+                 std::array<int, NDIMS> size,
+                 std::array<int, NDIMS> is_periodic) 
+        : _size        (size)
+        , _is_periodic (is_periodic) {
 
         // get communicator size and my rank
         MPI_Comm_size(comm, &_comm_size);
         MPI_Comm_rank(comm, &_comm_rank);
 
         // check processor grid size matches with communicator size
-        if (std::accumulate(proc_grid_size.begin(),
-                            proc_grid_size.end(),
+        if (std::accumulate(size.begin(),
+                            size.end(),
                             1, std::multiplies<int>()) != _comm_size)
             throw std::invalid_argument("incompatible processor count and processor grid size");
 
         // create communicator with cartesian topology
-        MPI_Cart_create(comm, NDIMS, _proc_grid_size.data(),
-                        _is_periodic.data(), false, &_comm);
+        MPI_Cart_create(comm, NDIMS, _size.data(), _is_periodic.data(), false, &_comm);
 
         // get cartesian coordinates of my rank
-        MPI_Cart_coords(_comm, _comm_rank, NDIMS, _proc_grid_coords.data());
+        MPI_Cart_coords(_comm, _comm_rank, NDIMS, _coords.data());
 
         // fill neighbours rank map
         for (auto region : HaloRegions<NDIMS>()) {
@@ -108,11 +107,11 @@ public:
 
     // ===================================================================== //
     // GET PROCESSOR GRID SIZE ALONG DIMENSION DIM
-    inline int grid_size(size_t dim) const {
+    inline int size(size_t dim) const {
         #if DARRAY_CONFIG_CHECKBOUNDS
             _checkboundsdim(dim);
         #endif
-        return _proc_grid_size[dim];        
+        return _size[dim];        
     }
 
     // ===================================================================== //
